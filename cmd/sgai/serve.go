94b7eaba82a008eeda422f6feccf28c8af658c5d
Britt Crawford <britt.v.crawford@gmail.com>
2026-02-03 15:17:24 -0800

    Add the goal composer flow and make it default if the user has not edited the goal

--- cmd/sgai/serve.go
+++ cmd/sgai/serve.go
@@ -440,6 +440,8 @@ func cmdServe(args []string) {
 	mux.HandleFunc("GET /workspaces/new", srv.handleNewWorkspaceGet)
 	mux.HandleFunc("POST /workspaces/new", srv.handleNewWorkspacePost)
 	mux.HandleFunc("/workspaces/", srv.routeWorkspace)
+	mux.HandleFunc("/compose", srv.routeCompose)
+	mux.HandleFunc("/compose/", srv.routeCompose)
 
 	log.Printf("sgai serve listening on http://%s", *listenAddr)
 	if err := http.ListenAndServe(*listenAddr, mux); err != nil {
@@ -642,6 +644,7 @@ type sessionData struct {
 	PMContent            template.HTML
 	ProjectMgmtContent   template.HTML
 	HasProjectMgmt       bool
+	HasEditedGoal        bool
 	CodeAvailable        bool
 	ActiveTab            string
 	SVGHash              string
@@ -651,13 +654,16 @@ type sessionData struct {
 
 func (s *Server) prepareSessionData(dir string, wfState state.Workflow, r *http.Request) sessionData {
 	goalContent := ""
+	hasEditedGoal := false
 	if data, err := os.ReadFile(filepath.Join(dir, "GOAL.md")); err == nil {
 		stripped := stripFrontmatter(string(data))
-		if rendered, err := renderMarkdown([]byte(stripped)); err == nil {
+		if rendered, errRender := renderMarkdown([]byte(stripped)); errRender == nil {
 			goalContent = rendered
 		} else {
 			goalContent = stripped
 		}
+		body := extractBody(data)
+		hasEditedGoal = len(strings.TrimSpace(string(body))) > 0
 	}
 
 	pmContent := ""
@@ -746,6 +752,7 @@ func (s *Server) prepareSessionData(dir string, wfState state.Workflow, r *http.
 		PMContent:            template.HTML(pmContent),
 		ProjectMgmtContent:   template.HTML(pmContent),
 		HasProjectMgmt:       projectMgmtExists,
+		HasEditedGoal:        hasEditedGoal,
 		CodeAvailable:        codeAvailable,
 		ActiveTab:            "goal",
 		SVGHash:              getWorkflowSVGHash(dir, currentAgent),
@@ -2373,6 +2380,12 @@ func (s *Server) renderWorkspaceContent(dir, tabName, sessionParam string, r *ht
 		}
 	}
 
+	hasEditedGoal := false
+	if data, err := os.ReadFile(filepath.Join(dir, "GOAL.md")); err == nil {
+		body := extractBody(data)
+		hasEditedGoal = len(strings.TrimSpace(string(body))) > 0
+	}
+
 	workspaceData := struct {
 		Directory            string
 		DirName              string
@@ -2392,6 +2405,7 @@ func (s *Server) renderWorkspaceContent(dir, tabName, sessionParam string, r *ht
 		FormattedModel       string
 		CurrentModel         string
 		ModelStatuses        map[string]string
+		HasEditedGoal        bool
 	}{
 		Directory:            dir,
 		DirName:              filepath.Base(dir),
@@ -2411,6 +2425,7 @@ func (s *Server) renderWorkspaceContent(dir, tabName, sessionParam string, r *ht
 		FormattedModel:       agentInfo.formattedModel,
 		CurrentModel:         wfState.CurrentModel,
 		ModelStatuses:        wfState.ModelStatuses,
+		HasEditedGoal:        hasEditedGoal,
 	}
 
 	var buf bytes.Buffer
