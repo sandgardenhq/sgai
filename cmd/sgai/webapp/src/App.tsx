e869c1c272518477df7eede72dfa48077201a9b1
U. Cirello <ulderico@sandgarden.com>
2026-02-16 22:21:37 -0800

    cmd/sgai/webapp: add browser native notifications for approval-needed events

--- cmd/sgai/webapp/src/App.tsx
+++ cmd/sgai/webapp/src/App.tsx
@@ -1,12 +1,17 @@
 import { Outlet } from "react-router";
 import { AppStateProvider } from "./contexts/AppStateProvider";
 import { ConnectionStatusBanner } from "./components/ConnectionStatusBanner";
+import { NotificationPermissionBar } from "./components/NotificationPermissionBar";
 import { TooltipProvider } from "./components/ui/tooltip";
+import { useNotifications } from "./hooks/useNotifications";
 
 export function App() {
+  useNotifications();
+
   return (
     <AppStateProvider>
       <TooltipProvider>
+        <NotificationPermissionBar />
         <ConnectionStatusBanner />
         <div className="min-h-screen bg-background text-foreground">
           <main className="p-4">
